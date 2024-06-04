40fbf4358448b634d386bf3714c2d57a8eb8e70f
GERM <gaston.mena.1982@gmail.com>
2024-06-04 15:13:33 -0300

    trabajando en el form de usuarios, con la imagen

--- views/usuarios/_form.php
+++ views/usuarios/_form.php
@@ -11,7 +11,7 @@ use kartik\widgets\FileInput;
 <div class="usuarios-form">
     <?php $form = ActiveForm::begin(); ?>
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-8">
             <div class="row">
                 <div class="col-md-12">
 
@@ -33,79 +33,84 @@ use kartik\widgets\FileInput;
                 </div>
             </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-4">
             <?php
-                if ($model->avatar == null) {
-                    echo $form->field($model, 'avatar', ['enableClientValidation' => true, 'enableAjaxValidation' => false])
-                        ->widget(FileInput::classname(), [
-                            //'name' => 'i1',
-                            'options' => ['accept' => 'image/*'],
-                            'language' => 'es',
-                            'pluginOptions' => [
-                                //'showPreview' => false,
-                                'allowedFileExtensions' => ['jpg', 'jpeg', 'gif', 'png', 'bmp'],
-                                'showCaption' => false,
-                                'showRemove' => false,
-                                'showUpload' => false,
-                                'showClose' => false,
-                                'showCancel' => false,
-                                'mainClass' => 'input-group-sm',
-                                'uploadUrl' => Url::to(['/mds_rum_novedad/update']),
-                                'maxFileSize' => 1000,
-                                /* 'initialPreview'=>[
-                                                    Html::img($model->Foto,['class'=>'file-preview-image']),
-                                                    ], */
-                                'previewFileType' => 'image',
-                                'initialCaption' => $model->avatar,
-                                'fileActionSettings' => [
+                if ($model->avatar == null) 
+                    {
+                        echo $form->field($model, 'avatar', ['enableClientValidation' => true, 
+                                                                'enableAjaxValidation' => false])
+                            ->widget(FileInput::classname(), [
+                                //'name' => 'i1',
+                                'options' => ['accept' => 'image/*'],
+                                'language' => 'es',
+                                'pluginOptions' => [
+                                    //'showPreview' => false,
+                                    'allowedFileExtensions' => ['jpg', 'jpeg', 'gif', 'png', 'bmp'],
+                                    'showCaption' => false,
                                     'showRemove' => false,
                                     'showUpload' => false,
-                                    'showZoom' => false,
-                                    'showCaption' => false,
-                                    'showCancel' => false
-                                ]
-                                //'minFileCount' => 1,
-                                // 'validateInitialCount' => true,
-                            ],
-                        ])->label('IMAGEN PRINCIPAL');
-                } else {
-                    echo $form->field($model, 'avatar', ['enableClientValidation' => true, 'enableAjaxValidation' => false])
-                        ->widget(FileInput::classname(), [
-                            'options' => ['accept' => 'image/*'],
-                            'language' => 'es',
-                            'pluginOptions' => [
-                                'allowedFileExtensions' => ['jpg', 'jpeg', 'gif', 'png', 'bmp'],
-                                'showCaption' => false,
-                                'showRemove' => false,
-                                'showUpload' => false,
-                                'showClose' => false,
-                                'showCancel' => false,
-                                'mainClass' => 'input-group-sm',
-                                'uploadUrl' => Url::to(['/mds_rum_novedad']),
-                                'maxFileSize' => 1000,
-                                'previewFileType' => 'image',
-                                'initialPreview' => [
-                                    //Html::img($model->avatar, ['class' => 'file-preview-image', 'style' => 'width:100%']),
-                                    Html::img(Url::base() . "/uploads/novedades/" . $model->avatar, ['class' => 'file-preview-image', 'style' => 'width:100%']),
-                                    //CHtml::image(Yii::app()->baseUrl."/uploads/ofertas/".$model->avatar);
+                                    'showClose' => false,
+                                    'showCancel' => false,
+                                    'mainClass' => 'input-group-sm',
+                                    'uploadUrl' => Url::to(['/mds_rum_novedad/update']),
+                                    'maxFileSize' => 1000,
+                                    /* 'initialPreview'=>[
+                                                        Html::img($model->Foto,['class'=>'file-preview-image']),
+                                                        ], */
+                                    'previewFileType' => 'image',
+                                    'initialCaption' => $model->avatar,
+                                    'fileActionSettings' => [
+                                        'showRemove' => false,
+                                        'showUpload' => false,
+                                        'showZoom' => false,
+                                        'showCaption' => false,
+                                        'showCancel' => false
+                                    ]
+                                    //'minFileCount' => 1,
+                                    // 'validateInitialCount' => true,
                                 ],
-                                'overwriteInitial' => true,
-                                'autoReplace' => true,
-                                'initialCaption' => $model->avatar,
-                                'fileActionSettings' => [
+                            ])->label('IMAGEN PRINCIPAL');
+                    } 
+                else 
+                    {
+                        $archivo = "/img/usuarios-avatares/$model->avatar.jpg";
+                        echo $form->field($model, 'avatar', ['enableClientValidation' => true, 'enableAjaxValidation' => false])
+                            ->widget(FileInput::classname(), [
+                                'options' => ['accept' => 'image/*'],
+                                'language' => 'es',
+                                'pluginOptions' => [
+                                    'allowedFileExtensions' => ['jpg', 'jpeg', 'gif', 'png', 'bmp'],
+                                    'showCaption' => false,
                                     'showRemove' => false,
                                     'showUpload' => false,
-                                    'showZoom' => false,
-                                    'showCaption' => false,
-                                    'showCancel' => false
+                                    'showClose' => false,
+                                    'showCancel' => false,
+                                    'mainClass' => 'input-group-sm',
+                                    'uploadUrl' => Url::to(['/mds_rum_novedad']),
+                                    'maxFileSize' => 1000,
+                                    'previewFileType' => 'image',
+                                    'initialPreview' => [
+                                        //Html::img($model->avatar, ['class' => 'file-preview-image', 'style' => 'width:100%']),
+                                        Html::img(Url::base() .$archivo, ['class' => 'file-preview-image', 'style' => 'width:100%']),
+                                        //CHtml::image(Yii::app()->baseUrl."/uploads/ofertas/".$model->avatar);
+                                    ],
+                                    'overwriteInitial' => true,
+                                    'autoReplace' => true,
+                                    'initialCaption' => $model->avatar,
+                                    'fileActionSettings' => [
+                                        'showRemove' => false,
+                                        'showUpload' => false,
+                                        'showZoom' => false,
+                                        'showCaption' => false,
+                                        'showCancel' => false
+                                    ]
+                                ],
+                                'pluginEvents' => [
+                                    "fileclear" => "function() { /*contempla evento de botón 'quitar' que se agrega al file browser*/ }",
+                                    "filereset" => "function() {  }",
                                 ]
-                            ],
-                            'pluginEvents' => [
-                                "fileclear" => "function() { /*contempla evento de botón 'quitar' que se agrega al file browser*/ }",
-                                "filereset" => "function() {  }",
-                            ]
-                        ])->label('IMAGEN PRINCIPAL');
-                }
+                            ])->label('IMAGEN PRINCIPAL');
+                    }
             ?>
         </div>
     </div>
