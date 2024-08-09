ff1cc9047e29c441d42a6189e807ab74beeef000
Ivan <ivansueldo1981@gmail.com>
2024-08-09 10:06:38 -0300

    modulo articulo listo para usar. falta desarrollar imagen , pero no es impedimento para la carga

--- views/articulo/_columns.php
+++ views/articulo/_columns.php
@@ -12,8 +12,19 @@ order by descripcion";
 $mysql_marca = "SELECT * from configuracion where activo=1 and id_configuracion_tipo = 14
 order by descripcion";
 
+$mysql_rubro = "SELECT * from configuracion where activo=1 and id_configuracion_tipo = 15
+order by descripcion";
+
+$mysql_unidad_medida = "SELECT * from configuracion where activo=1 and id_configuracion_tipo = 13
+order by descripcion";
+$columna_1 = '15%' ;
+$columna_2 = '15%';
+$columna_3 = '15%';
+$columna_4 = '15%';
+$columna_5 = '15%';
+$columna_6 = '10%';
 return [
-    [
+   /*  [
         'class' => 'kartik\grid\CheckboxColumn',
         'width' => '20px',
     ],
@@ -24,15 +35,13 @@ return [
         [
         'class'=>'\kartik\grid\DataColumn',
         'attribute'=>'idarticulo',
-    ],
-    [
-        'class'=>'\kartik\grid\DataColumn',
-        'attribute'=>'descripcion',
-    ],
+    ], */
+    
    
     [
         'class' => '\kartik\grid\DataColumn',
         'attribute' => 'idtipo',
+        'width' => $columna_1,
         'value' => function ($model) {
             $id = $model->idtipo;
             if ($id != null) {
@@ -42,7 +51,7 @@ return [
             return "";
         },
         'filterType' => GridView::FILTER_SELECT2,
-        'filter' => ArrayHelper::map(Configuracion::findBySql($mysql_tipo)->all(), 'id_configuracion_tipo', 'descripcion'),
+        'filter' => ArrayHelper::map(Configuracion::findBySql($mysql_tipo)->all(), 'id_configuracion', 'descripcion'),
         'filterWidgetOptions' => [
             'pluginOptions' => ['allowClear' => true],
         ],
@@ -53,6 +62,7 @@ return [
     [
         'class' => '\kartik\grid\DataColumn',
         'attribute' => 'idmarca',
+        'width' => $columna_2,
         'value' => function ($model) {
             $id = $model->idmarca;
             if ($id != null) {
@@ -62,7 +72,7 @@ return [
             return "";
         },
         'filterType' => GridView::FILTER_SELECT2,
-        'filter' => ArrayHelper::map(Configuracion::findBySql($mysql_marca)->all(), 'id_configuracion_tipo', 'descripcion'),
+        'filter' => ArrayHelper::map(Configuracion::findBySql($mysql_marca)->all(), 'id_configuracion', 'descripcion'),
         'filterWidgetOptions' => [
             'pluginOptions' => ['allowClear' => true],
         ],
@@ -72,15 +82,53 @@ return [
     [
         'class'=>'\kartik\grid\DataColumn',
         'attribute'=>'modelo',
+        'width' => $columna_3,
+        
     ],
-    // [
-        // 'class'=>'\kartik\grid\DataColumn',
-        // 'attribute'=>'idrubro',
-    // ],
-    // [
-        // 'class'=>'\kartik\grid\DataColumn',
-        // 'attribute'=>'id_unidad_medida',
-    // ],
+   /*  [
+        'class'=>'\kartik\grid\DataColumn',
+        'attribute'=>'descripcion',
+    ], */
+    [
+        'class' => '\kartik\grid\DataColumn',
+        'attribute' => 'idrubro',
+        'width' => $columna_4,
+        'value' => function ($model) {
+            $id = $model->idrubro;
+            if ($id != null) {
+                $tipo = Configuracion::findOne($id);
+                return "$tipo->descripcion";
+            }
+            return "";
+        },
+        'filterType' => GridView::FILTER_SELECT2,
+        'filter' => ArrayHelper::map(Configuracion::findBySql($mysql_rubro)->all(), 'id_configuracion', 'descripcion'),
+        'filterWidgetOptions' => [
+            'pluginOptions' => ['allowClear' => true],
+        ],
+        'filterInputOptions' => ['placeholder' => 'Rubro...'],
+        'format' => 'raw',
+    ],
+    [
+        'class' => '\kartik\grid\DataColumn',
+        'attribute' => 'id_unidad_medida',
+        'width' => $columna_5,
+        'value' => function ($model) {
+            $id = $model->id_unidad_medida;
+            if ($id != null) {
+                $tipo = Configuracion::findOne($id);
+                return "$tipo->descripcion";
+            }
+            return "";
+        },
+        'filterType' => GridView::FILTER_SELECT2,
+        'filter' => ArrayHelper::map(Configuracion::findBySql($mysql_unidad_medida)->all(), 'id_configuracion', 'descripcion'),
+        'filterWidgetOptions' => [
+            'pluginOptions' => ['allowClear' => true],
+        ],
+        'filterInputOptions' => ['placeholder' => 'Unidad de Medida...'],
+        'format' => 'raw',
+    ],
     // [
         // 'class'=>'\kartik\grid\DataColumn',
         // 'attribute'=>'activo',
@@ -93,9 +141,11 @@ return [
         'class' => 'kartik\grid\ActionColumn',
         'dropdown' => false,
         'vAlign'=>'middle',
+        'width' => $columna_6,
         'urlCreator' => function($action, $model, $key, $index) { 
                 return Url::to([$action,'id'=>$key]);
         },
+        'template' => '{view} {update} ',
         'viewOptions'=>['role'=>'modal-remote','title'=>'View','data-toggle'=>'tooltip'],
         'updateOptions'=>['role'=>'modal-remote','title'=>'Update', 'data-toggle'=>'tooltip'],
         'deleteOptions'=>['role'=>'modal-remote','title'=>'Delete', 
