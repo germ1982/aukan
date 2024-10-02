691565f600241f12b87105cbbd9f8281ad78a40f
GERM <gaston.mena.1982@gmail.com>
2024-10-02 15:10:14 -0300

    se esta trabajando en stock

--- views/stock_informatica_ingreso/_form.php
+++ views/stock_informatica_ingreso/_form.php
@@ -1,35 +1,40 @@
 <?php
+
+use app\controllers\SiteController;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\StockInformaticaIngreso */
-/* @var $form yii\widgets\ActiveForm */
+$model->fecha = date('d/m/Y', strtotime($model->fecha));
 ?>
 
-<div class="stock-informatica-ingreso-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <?= $form->field($model, 'idusuario_carga')->hiddenInput(['id' => 'input_idusuario_carga'])->label(false) ?>
+    <div class="row">
+        <div class="col-md-3">
+            <?= SiteController::actionGet_input_fecha($form, $model, 'fecha', 'fecha', 'Fecha') ?>
+        </div>
+        <div class="col-md-6">
+            <?= $form->field($model, 'idempleado_recepcion')->textInput() ?>
+        </div>
 
-    <?= $form->field($model, 'fecha')->textInput() ?>
+    </div>
 
-    <?= $form->field($model, 'idorigen')->textInput() ?>
+    <div class="row">
+        <div class="col-md-4">
+            <?= $form->field($model, 'idorigen')->textInput() ?>
+        </div>
+        <div class="col-md-8">
+            <?= $form->field($model, 'origen_referencia')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
 
-    <?= $form->field($model, 'origen_referencia')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'idempleado_recepcion')->textInput() ?>
-
-    <?= $form->field($model, 'idusuario_carga')->textInput() ?>
-
-    <?= $form->field($model, 'observacion')->textInput(['maxlength' => true]) ?>
-
-  
-	<?php if (!Yii::$app->request->isAjax){ ?>
-	  	<div class="form-group">
-	        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-	    </div>
-	<?php } ?>
+    <div class="row">
+        <div class="col-md-12">
+            <?= $form->field($model, 'observacion')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
 
     <?php ActiveForm::end(); ?>
-    
-</div>
+
