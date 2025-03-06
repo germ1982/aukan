47430153d109e6253b39892bef9ac41dea5a3807
Ivan <ivansueldo1981@gmail.com>
2025-03-06 12:29:49 -0300

    index del modulo runneu_legajo

---
+++ views/runneu_legajo/view.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\RunneuLegajo */
+?>
+<div class="runneu-legajo-view">
+ 
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'nº_legajo',
+            'dni',
+            'archivo_adjunto',
+        ],
+    ]) ?>
+
+</div>
