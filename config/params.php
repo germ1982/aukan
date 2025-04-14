836e60b13589431ec7fbf30a5d814fef9f2450ad
GERM <gaston.mena.1982@gmail.com>
2025-04-14 11:27:19 -0300

    se cambia params a unidad E

--- config/params.php
+++ config/params.php
@@ -4,5 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'rutaUploads' => 'D:/uploads_datafam/',
+    'rutaUploads' => 'E:/uploads_datafam/',
 ];
